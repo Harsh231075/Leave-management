dc6ff49ae758dc9296bd64b7efb29d7a7805e0cf
harsh231075 <harshsingh231075@gmail.com>
2026-01-16 19:52:28 +0530

    change layout

--- client/src/components/layout/DashboardLayout.tsx
+++ client/src/components/layout/DashboardLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
 
 interface DashboardLayoutProps {
   role: "employee" | "admin";
@@ -11,8 +10,7 @@ const DashboardLayout = ({ role, title }: DashboardLayoutProps) => {
   return (
     <div className="flex min-h-screen w-full bg-background">
       <Sidebar role={role} isOpen={true} />
-      <div className="flex-1 flex flex-col lg:ml-0">
-        <Navbar title={title} />
+      <div className="flex-1 flex flex-col lg:ml-64">
         <main className="flex-1 overflow-auto">
           <Outlet />
         </main>
